b270b7098e03a15a38d59f0c1c426bbd1ddcc922
vilsonlu <vilson.espayos.lu@gmail.com>
2018-08-04 05:20:06 +0800

    Working with Data : Validations

--- ProjectMVC/Controllers/ReviewsController.cs
+++ ProjectMVC/Controllers/ReviewsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -17,5 +18,47 @@ namespace ProjectMVC.Controllers
             return View(restaurantReview);
         }
 
+        public ActionResult Create(int restaurantId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(RestaurantReview review)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Reviews.Add(review);
+                _db.SaveChanges();
+                return RedirectToAction("Index", new {id = review.RestaurantId});
+            }
+
+            return View(review);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var review = _db.Reviews.Find(id);
+            return View(review);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(RestaurantReview review)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(review).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index", new { id = review.RestaurantId });
+            }
+
+            return View(review);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+             _db.Dispose();
+            base.Dispose(disposing);
+        }
     }
 }
